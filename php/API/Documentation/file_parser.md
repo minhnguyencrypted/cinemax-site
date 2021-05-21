6100cf57747086f60366385b33e47778782eeb2c
Minh Nguyen <minhnguyen1205@outlook.com>
2021-05-22 01:30:38 +0700

    Add new functions to header and update markdown
    - Add two functions: match_object_by_value() and match_object_by_list()
    for matching objects by category value.
    - Update documentation markdown according to added functions.
    - Edit some functions' arguments and body.

--- php/API/Documentation/file_parser.md
+++ php/API/Documentation/file_parser.md
@@ -8,48 +8,86 @@ This eases the process of retrieving data and filter items according to categori
 loading data from CSV file.
 
 ## Contents of the header:
-- Constants:
-    - `SEPARATOR = ','` : comma (`,`) is used in .CSV (**Comma**-Separated Values) files for separating values, this
-    constant is used in function `explode()` for splitting CSV lines.
-    - `READ_MODE = 'r'` : function `fopen()` takes `r` as the open file for reading mode.
+### Constants:
+- `SEPARATOR = ','` : comma (`,`) is used in .CSV (**Comma**-Separated Values) files for separating values, this
+constant is used in function `explode()` for splitting CSV lines.
+- `READ_MODE = 'r'` : function `fopen()` takes `r` as the open file for reading mode.
 
 
-- Functions
-    - `parse_line()` : Parse CSV-formatted string and return the object as an associative/indexed array.
-        - Arguments:
-            - `$line` : The CSV string to be parsed.
-            - `$categories` : (optional) The array of object's categories, object will be returned as an indexed array.
-        - Return values: Return an indexed/associative array if string is parsable, otherwise, return `false`
-        - Undefined behaviours/errors: This situation may happen when passing a non-string value to `$line` argument and
-          passing non-array value to
-            `$categories` argument.
- 
-    - `read_file()`: Read user-specified .CSV file and parse its contents (all contents or only those matches
-      specified value of category) into an array of objects.
-        - Arguments:
-            - `$file_path`: The path to the CSV file to be parsed.
-            - `$filter_category`: (optional) The category (aka attribute) of object to be considered when filtering.
-            - `$filter_value`: (optional) The value of object's category to filter.
-        - Return values: When passed with `$filter_category` and `$filter_value`, function only returns an array
-        contains all objects matched the category value. Otherwise, the function will return every object it is able to
-        parse from the CSV file. `false` may be returned when the CSV file was not open successfully or no objects could
-        not be parsed.
-        - Undefined behaviours/errors: This situation may happen when passing a non-string value to the last two
-          arguments.
-    - `get_categories()`: Retrieve CSV file categories
-        - Arguments:
-            - `$file_path`: The path to the CSV file to be retrieved.
-        - Return values: If it's successful, the functions returns an array of categories defined in the CSV file.
-          Otherwise, `false` may be returned in case of no categories can be found or file could not open successfully.
-        - Undefined behaviours/errors: No Undefined behaviours/errors found yet.
-    - `fgets_clean()`: It behaves the same to `fgets()`, but returns the string without the newline character at the end
-      of the string. Arguments and Return values are the same to `fgets()`.
-      
+### Functions:
+- `parse_line()` : Parse CSV-formatted string and return the object as an associative/indexed array.
+    - Arguments:
+        - `$line` : The CSV string to be parsed.
+        - `$categories` : (optional) The array of object's categories, object will be returned as an indexed array.
+    - Return values: Return an indexed/associative array if string is parsable, otherwise, return `false`
+    - Undefined behaviours/errors: This situation may happen when passing a non-string value to `$line` argument and
+      passing non-array value to
+        `$categories` argument.
+
+- `read_all_file()`: Read user-specified .CSV file and parse its contents into an array of objects.
+    - Arguments:
+        - `$file_path`: The path to the CSV file to be parsed.
+    - Return values: Return an array of objects (objects' attributes and values stored as an associative array),
+      return an empty array when no objects can be parsed. If file cannot be opened successfully, return `false`
+    - Undefined behaviours/errors: No undefined behaviours/errors found yet.
+  
+- `read_file_match_value()`: Read user-specified CSV file and parse its contents, only ones which matches a specified
+  value of specified category will be added to the return array.
+    - Arguments:
+        - `$file_path`: The path to the CSV file to be parsed.
+        - `$match_value`: The matching value for an specified object's category (if `$match_category` is passed) or
+          any categories.
+        - `$match_category`: (optional) The category to match the value with, if not passed, any categories has the
+          same value to `$match_value`, object will be considered matched.
+    - Return values: Return non-empty an indexed array of matched objects, if find any, otherwise, an 
+      empty one will be returned. If the CSV file cannot be opened, return `false`.
+    - Undefined behaviours/errors: No undefined behaviour/errors found yet.
+
+- `read_file_match_list()`: Read user-specified CSV file and parse its contents, only ones which matches a number of
+  specified values of categories will be added to the return array.
+    - Arguments:
+        - `$file_path`: The path to the CSV file to be parsed.
+        - `$match_values`: The matching values for the certain object's category stored in `$match_categories` (if it is passed) or
+        any categories otherwise.
+        - `$match_categories`: (optional) The list of categories of the object need to be matched with respectively indexed values in `$match_values`
+    - Return values: Return a non-empty indexed array of matched objects, if find any, otherwise, an empty one will be
+      returned. If the CSV file cannot be opened, return `false`.
+    - Undefined behaviours/errors: No undefined behaviour/errors found yet.
+
+- `match_object_by_value()`: Decide whether the object has a certain category matches a specified value.
+    - Arguments:
+        - `$object`: The associative array of object.
+        - `$match_value`: The string of category value to check for matching, if `$match_category` is not passed, try to
+          match such value with every object's categories.
+        - `$match_category`: (optional) The name of the category to be checked if it's value matches the `$match_value`.
+    - Return values: Return a non-empty indexed array of matched objects, if find any, otherwise, an empty one will be
+    returned. If the CSV file cannot be opened, return `false`.
+    - Undefined behaviours/errors: No undefined behaviour/errors found yet.
+
+- `match_object_by_list()`: Decide whether the object has a number of specified categories matches several values.
+    - Arguments:
+        - `$object`: The associative array of object.
+        - `$match_values`: The array of strings as a value of same-index category stored in `$match_categories` to be checked, if it is not passed, try to
+          match such value with every object's categories.
+        - `$match_categories`: (optional) The list of object categories to be checked if the values matches the corresponding on in `$match_values`.
+    - Return values: Return a non-empty indexed array of matched objects, if find any, otherwise, an empty one will be
+      returned. If the CSV file cannot be opened, return `false`.
+    - Undefined behaviours/errors: No undefined behaviour/errors found yet.
+    
+- `get_categories()`: Retrieve CSV file categories
+    - Arguments:
+        - `$file_path`: The path to the CSV file to be retrieved.
+    - Return values: If it's successful, the functions returns an array of categories defined in the CSV file.
+    Otherwise, `false` may be returned in case of no categories can be found or file could not open successfully.
+    - Undefined behaviours/errors: No Undefined behaviours/errors found yet.
+
+- `fgets_clean()`: It behaves the same to `fgets()`, but returns the string without the newline character at the end
+of the string. Arguments and Return values are the same to `fgets()`.
+  
 ## Information about this header
 Initially written by Minh Nguyen
 
 Path (from repository root): `php/API/data_loader/file_parser.php`  
-
 
 Use `require_once()` to include this header into your code, specify the relative path corresponding to your current
 file's location.  
--- php/API/data_loader/file_parser.php
+++ php/API/data_loader/file_parser.php
@@ -6,7 +6,7 @@
         return preg_replace("/[\n]$/","",fgets($file_obj));
     }
 
-    function parse_line($line, $categories = []) {
+    function parse_line(string $line, array $categories = []) {
         //Split line into array
         $line_split = explode(SEPARATOR,$line);
         if (!$line_split || empty($line)) {
@@ -21,13 +21,14 @@
                     $parsed_object[$i++] = $line_value;
                 }
             } else {
-                $i = 0;
-                foreach ($line_split as $line_value) {
-                    $parsed_object[$categories[$i++]] = $line_value;
+                //If the number of fields parsed is less than the number categories, return false
+                if (count($line_split) < count ($categories)) {
+                    return false;
+                }
+                for ($i = 0; $i < count($categories); $i++) {
+                    $parsed_object[$categories[$i]] = $line_split[$i];
                 }
             }
-        } else {
-            $parsed_object[$categories] = $line_split;
         }
         return $parsed_object;
     }
@@ -39,6 +40,11 @@
         var_dump(line_parser("foo",""));
         var_dump(line_parser("foo",null));
         var_dump(line_parser("foo,bar,baz,qux"));
+    var_dump(parse_line("foo,bar,baz",['name','id','group']));
+    var_dump(parse_line("foo,bar,baz",['name']));
+    var_dump(parse_line("foo,barbaz",['name','id','group']));
+    var_dump(parse_line("",['name','id','group']));
+    var_dump(parse_line("    ",['name']));
     *
     */
 
@@ -49,8 +55,13 @@
             return false;
         }
 
-        //Return categories list
-        return parse_line(fgets_clean($file_obj));
+        do {
+            $categories = parse_line(fgets_clean($file_obj));
+        } while (!feof($file_obj) && $categories === false);
+
+        //Close file and return categories list
+        fclose($file_obj);
+       return $categories;
     }
     /*
      * get_categories() test cases, ignore these lines
@@ -61,54 +72,178 @@
      *
      */
 
-    function read_file($file_path, $filter_category = "", $filter_value = "") {
+    function match_object_by_value(array $object, string $match_value, string $match_category = "") : bool {
+        //If match_category is blank, try to match every objects that has any value of $match_value
+        //Otherwise, match specified category with the specified value
+        if ($match_category === "") {
+            foreach ($object as $object_value) {
+                if ($object_value === $match_value) {
+                    return true;
+                }
+            }
+            return false;
+        } else {
+            return isset($object[$match_category]) ?
+                $object[$match_category] === $match_value : false;
+        }
+    }
+
+    /*
+     * Test cases for filter_object(), ignore these lines
+        $foo = ["name" => "foo", "id" => "bar", "group" => "123"];
+        var_dump(match_object($foo,"name","foo"));
+        var_dump(match_object($foo,"","foo"));
+        var_dump(match_object($foo,"","123"));
+        var_dump(match_object($foo,"",""));
+        var_dump(match_object($foo,"id","foo"));
+        var_dump(match_object($foo,"id"));
+     *
+     */
+
+    function match_object_by_list(array $object,  array $match_values, array $match_categories = []) : bool {
+        //If match_categories is blank, try to match every categories with every values of $match_values,
+        //Otherwise, perform matching categories with their corresponding match value
+        if (empty($match_categories)) {
+            foreach ($object as $object_category) {
+                foreach ($match_values as $value) {
+                    if ($object_category === $value) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        } else {
+            for ($i = 0; $i < count($match_categories); $i++) {
+                if (!isset($object[$match_categories[$i]]) || $object[$match_categories[$i]] !== $match_values[$i]) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    /*
+     * Test cases for match_objet_by_list(), ignore these lines
+    $foo = ['name' => 'foo', 'id' => 'bar', 'group' => 'bar', 'number' => '0123'];
+    $bar = ['name'=> 'baz', 'id' => 'qux', 'group' => 'quux', 'number' => '12'];
+    var_dump(match_object_by_list($foo));
+    var_dump(match_object_by_list($foo,['name','id']));
+    var_dump(match_object_by_list($foo,['name','id'],['foo','bar']));
+    var_dump(match_object_by_list($foo,[],['foo','bar']));
+    var_dump(match_object_by_list($bar,[],['foo','bar']));
+    var_dump(match_object_by_list($bar,['name'],['baz','bar']));
+    var_dump(match_object_by_list($bar,['name'],['bar','baz']));
+    var_dump(match_object_by_list($bar,['name','id','group','foo'],['baz','qux','quux']));
+    var_dump(match_object_by_list($bar,['name','id','group'],['baz','qux','quux']));
+    var_dump(match_object_by_list($bar,['name','bar'],['baz','bar']));
+     *
+     */
+
+    function read_all_file(string $file_path) {
         //Prepare file for reading
         $file_obj = fopen($file_path,READ_MODE);
         if ($file_obj === false) {
             return false;
         }
-        //Perform file reading
+
         //Retrieve categories list
-        $categories = parse_line(fgets_clean($file_obj));
+        do {
+            $categories = parse_line(fgets_clean($file_obj));
+        } while (!feof($file_obj) && $categories === false);
 
         $objects_array = [];
-        //Dealing with optional arguments
-        if ($filter_category !== "" || $filter_value !== "") {
-            //Arguments: ($file_path,<string>,<string>)
-            while (!feof($file_obj)) {
-                $current_obj = parse_line(fgets_clean($file_obj), $categories);
-                if ($current_obj !== false && isset($current_obj[$filter_category]) &&
-                    $current_obj[$filter_category] === $filter_value) {
-                    $objects_array[] = $current_obj;
-                }
-            }
-        } else {
-            //Arguments: ($file_path,"","")
-            while (!feof($file_obj)) {
-                $current_obj = parse_line(fgets_clean($file_obj), $categories);
-                if ($current_obj !== false) {
-                    $objects_array[] = $current_obj;
-                }
+        while (!feof($file_obj)) {
+            $current_obj = parse_line(fgets_clean($file_obj),$categories);
+            if ($current_obj !== false) {
+                $objects_array[] = $current_obj;
             }
         }
 
-        return empty($objects_array) ? false : $objects_array;
+        //Close file and return objects list
+        fclose($file_obj);
+        return $objects_array;
     }
 
     /*
-     * Test cases for read_file(), ignore these lines
-        var_dump(read_file("tests/foo.csv","name","foo"));
-        var_dump(read_file("tests/bar.csv","name","foo"));
-        var_dump(read_file("tests/bar.csv","foo","foo"));
-        var_dump(read_file("tests/baz.csv","address","foo"));
+     * Test cases for read_all_file(), ignore these lines
+    var_dump(read_all_file("tests/foo.csv"));
+     *
+     */
+
+    function read_file_match_value(string $file_path, string $match_value, string $match_category = "") {
+        //Prepare file for reading
+        $file_obj = fopen($file_path,READ_MODE);
+        if ($file_obj === false) {
+            return false;
+        }
+
+        //Retrieve categories list
+        do {
+            $categories = parse_line(fgets_clean($file_obj));
+        } while (!feof($file_obj) && $categories === false);
+
+        $objects_array = [];
+        while (!feof($file_obj)) {
+            $current_obj = parse_line(fgets_clean($file_obj),$categories);
+            if ($current_obj !== false && match_object_by_value($current_obj, $match_value, $match_category)) {
+                $objects_array[] = $current_obj;
+            }
+        }
+
+        //Close file and return objects list
+        fclose($file_obj);
+        return $objects_array;
+    }
+
+    /*
+     * Test cases for read_file_match_value(), ignore these lines
+        var_dump(read_file_match_value("tests/foo.csv","name","foo"));
+        var_dump(read_file_match_value("tests/bar.csv","name","foo"));
+        var_dump(read_file_match_value("tests/bar.csv","foo","foo"));
+        var_dump(read_file_match_value("tests/baz.csv","address","foo"));
         echo "qux ";
-        var_dump(read_file("tests/qux.csv","address","foo"));
-        var_dump(read_file("tests/quux.csv","address","foo"));
-        var_dump(read_file("tests/quuz.csv","group","TRUE"));
-        var_dump(read_file("tests/foo.csv"));
-        var_dump(read_file("tests/foo.csv","name","foo"));
-        var_dump(read_file("tests/foo.csv",["name"],["foo"]));
-        var_dump(read_file("tests/foo.csv"));
-        var_dump(read_file("tests/foo.csv","name","foo"));
+        var_dump(read_file_match_value("tests/qux.csv","address","foo"));
+        var_dump(read_file_match_value("tests/quux.csv","address","foo"));
+        var_dump(read_file_match_value("tests/quuz.csv","group","TRUE"));
+        var_dump(read_file_match_value("tests/foo.csv"));
+        var_dump(read_file_match_value("tests/foo.csv","name","foo"));
+        var_dump(read_file_match_value("tests/foo.csv",["name"],["foo"]));
+        var_dump(read_file_match_value("tests/foo.csv"));
+        var_dump(read_file_match_value("tests/foo.csv","name","foo"));
+     *
+     */
+
+    function read_file_match_list(string $file_path, array $match_values, array $match_categories = []) {
+        //Prepare file for reading
+        $file_obj = fopen($file_path,READ_MODE);
+        if ($file_obj === false) {
+            return false;
+        }
+
+        //Retrieve categories list
+        do {
+            $categories = parse_line(fgets_clean($file_obj));
+        } while (!feof($file_obj) && $categories === false);
+
+        //If category argument is blank
+        $objects_array = [];
+        while (!feof($file_obj)) {
+            $current_obj = parse_line(fgets_clean($file_obj),$categories);
+            if ($current_obj !== false && match_object_by_list($current_obj, $match_values, $match_categories)) {
+                $objects_array[] = $current_obj;
+            }
+        }
+
+        //Close file and return objects list
+        fclose($file_obj);
+        return $objects_array;
+    }
+
+    /*
+     * Test cases for read_file_match_list(), ignore these lines
+        var_dump(read_file_match_list("tests/foo.csv",['name','id'],['foo','qux']));
+        var_dump(read_file_match_list("tests/foo.csv",[],['foo','qux']));
+        var_dump(read_file_match_list("tests/foo.csv",[],['foo','qux']));
+        var_dump(read_file_match_list("tests/foo.csv",[],[]));
      *
      */
